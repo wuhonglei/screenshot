8091a99c31c33b2624dad6ac98deae1f66ae91c9
wuhonglei <1017368065@qq.com>
2020-05-26 23:58:47 +0800

    完成了本地存储

--- public/index.js
+++ public/index.js
@@ -15,6 +15,17 @@ function setImage(url) {
     img.src = url;
 }
 
+// 存储数据
+function setStore(url, data) {
+    var uri = new URL(url, location.href),
+        site = uri.searchParams.get('site'),
+        _uri = new URL(site),
+        hostname = _uri.hostname,
+        item = Object.assign({ hostname }, data);
+
+    localStorage.setItem(hostname, JSON.stringify(item));
+}
+
 function sendRequest() {
     var btn = document.querySelector('#button-addon2'),
         input = document.querySelector('input');
@@ -32,6 +43,7 @@ function sendRequest() {
         let url = `./screenshot?site=${value}`;
         ajax(url, function(data) {
             setImage(data.pagePath);
+            setStore(url, data);
         });
     }
 }
